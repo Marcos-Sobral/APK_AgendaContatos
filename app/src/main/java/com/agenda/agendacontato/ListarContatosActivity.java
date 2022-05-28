1067a67b4d242404b62845c6afab8859079467aa
Marcos Sobral <vini15oficial@gmail.com>
2022-05-28 18:46:10 -0300

    criar e ver contato, menu superior e navigation

--- app/src/main/java/com/agenda/agendacontato/ListarContatosActivity.java
+++ app/src/main/java/com/agenda/agendacontato/ListarContatosActivity.java
@@ -2,7 +2,11 @@ package com.agenda.agendacontato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,7 +29,25 @@ public class ListarContatosActivity extends AppCompatActivity {
         dao = new ContatoDAO(this);
         contatos = dao.obterTodos();
         contatosFiltrado.addAll(contatos);
-        ArrayAdapter<Contato> adaptador = new ArrayAdapter<Contato>(this, android.R.layout.simple_list_item_1, contatos);
+        ArrayAdapter<Contato> adaptador = new ArrayAdapter<Contato>(this, android.R.layout.simple_list_item_1, contatosFiltrado);
         listView.setAdapter(adaptador);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+    public void cadastrar(MenuItem item){
+        Intent  it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+
+    @Override
+    public void onResume(){
+       super.onResume();
+       contatos = dao.obterTodos();
+       contatosFiltrado.clear();
+       contatosFiltrado.addAll(contatos);
+       listView.invalidateViews();
     }
 }
